a2bef3ec4e834fc3e7e7f1a64ff5d773b5cb5b6c
jvhariharan <jvhariharan@gmail.com>
2021-01-28 16:25:52 +0530

    EJ2 MVC Grid server exporting

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Syncfusion.EJ2.Grids;
+using Syncfusion.EJ2.GridExport;
+using GridExportMVC.Models;
+
+namespace GridExportMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var Order = OrdersDetails.GetAllRecords();
+            ViewBag.DataSource = Order;
+            return View();
+        }
+        public ActionResult ExcelExport(string gridModel)
+        {
+            GridExcelExport exp = new GridExcelExport();
+            Grid gridProperty = ConvertGridObject(gridModel);
+            return exp.ExcelExport<OrdersDetails>(gridProperty, OrdersDetails.GetAllRecords());
+        }
+        public ActionResult PdfExport(string gridModel)
+        {
+            GridPdfExport exp = new GridPdfExport();
+            Grid gridProperty = ConvertGridObject(gridModel);
+            return exp.PdfExport<OrdersDetails>(gridProperty, OrdersDetails.GetAllRecords());
+        }
+
+        private Grid ConvertGridObject(string gridProperty)
+        {
+            Grid GridModel = (Grid)Newtonsoft.Json.JsonConvert.DeserializeObject(gridProperty, typeof(Grid));
+            GridColumnmodel cols = (GridColumnmodel)Newtonsoft.Json.JsonConvert.DeserializeObject(gridProperty, typeof(GridColumnmodel));
+            GridModel.Columns = cols.columns;
+            return GridModel;
+        }
+        public class GridColumnModel
+        {
+            public List<GridColumn> columns { get; set; }
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GridExportMVC.MvcApplication" Language="C#" %>
